c58b38420993f114a896ffaf983c6f8185ff2030
abiralaoui <abiralaoui258@gmail.com>
2024-01-31 13:27:28 +0100

    lite topic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Topic } from '../models/topic';
+import { TopicService } from '../services/topic.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  topics: Topic[] = [];
 
-  constructor() {}
 
+  constructor( private topicService: TopicService) {}
+
+  ngOnInit() {
+    // Appel de la méthode de récupération des Topics lors de l'initialisation de la page
+
+    this.getTopics();
+  }
+
+  // Méthode pour récupérer les Topics à partir du service
+  getTopics() {
+    this.topics = this.topicService.getTopics();
+    // Traiter les données récupérées, par exemple, les stocker dans une propriété du composant dbase
+  }
 }
+
+
+
+
